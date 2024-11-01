1bcef02832504823aa559f6ab6e301899204341e
Nicolas Nagano <nicolasjap@hotmail.com>
2024-11-01 12:56:07 -0300

    feat: add order

--- apps/backend/src/product/product.module.ts
+++ apps/backend/src/product/product.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { DbModule } from 'src/db/db.module';
-import { ProductRepositoryTs } from './product.repository.ts';
+import { ProductRepositoryTs } from './product.repository';
 
 @Module({
   imports: [DbModule],
