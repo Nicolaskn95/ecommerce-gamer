2a0d8ee20bcae3fd7e949fd396c21cc1237e1874
Nicolas Nagano <nicolasjap@hotmail.com>
2024-11-22 10:12:57 -0300

    chore: created hooks

--- apps/frontend/src/data/hooks/useInstallment.ts
+++ apps/frontend/src/data/hooks/useInstallment.ts
@@ -1,6 +1,6 @@
 import { InstallmentCalc } from "@gstore/core";
 
-export default function useInstallment(value: number, quantity: number = 1) {
+export default function useInstallment(value: number, quantity: number = 12) {
   const installment = new InstallmentCalc().implement(value, quantity);
   return installment;
 }
